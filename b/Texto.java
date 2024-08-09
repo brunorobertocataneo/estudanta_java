8d2c9a075a7ae944e2c127e7bce62404b92881f1
brunorobertocataneo <149379924+brunorobertocataneo@users.noreply.github.com>
2024-08-09 08:03:55 -0300

    Add files via upload

---
+++ b/Texto.java
@@ -0,0 +1,14 @@
+public class Texto {
+
+    public static void main(String[] args) {
+        String s = new String("IFC Videira"); // Forma tradicional de criar objetos
+        String s2; //Declaração
+        s2 = "IFC VIdeira"; //iniciação
+        String s3 = "IFC Videira"; // Decalração com iniciação
+
+        String sub = s.substring(0,5);
+
+        System.out.println(sub);
+
+    }
+}
